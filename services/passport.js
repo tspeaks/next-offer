65d011170bd10a309d648f1ca954ff6ec77e1f0f
Tyler Speaks <speaks_tyler@yahoo.com>
2018-02-10 17:00:18 -0600

    complete auth flow

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,19 @@
 const passport = require("passport");
 const googleStrat = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
 const keys = require("../config/keys");
+
+const User = mongoose.model("users");
+
+passport.serializeUser((user, done) => {
+	done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+	User.findById(id).then(user => {
+		done(null, user);
+	});
+});
 
 passport.use(
 	new googleStrat(
@@ -10,11 +23,16 @@ passport.use(
 			callbackURL: "/auth/google/callback"
 		},
 		(accessToken, refreshToken, profile, done) => {
-			console.log("access token", accessToken);
-			console.log("refresh token", refreshToken);
-			console.log("profile", profile);
+			User.findOne({ googleId: profile.id }).then(existingUser => {
+				if (existingUser) {
+					//preexisting record
+					done(null, existingUser);
+				} else {
+					new User({ googleId: profile.id })
+						.save()
+						.then(user => done(null, user));
+				}
+			});
 		}
 	)
 );
-
-
